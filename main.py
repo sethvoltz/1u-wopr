f8c8d7223ccf49ff0e0f90e11902cec4bf35a319
Seth Voltz <seth@designgods.net>
2025-03-02 18:15:10 -0800

    feat: move to async for real time delta checks

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
 from machine import Pin, SPI
 import max7219
 import random
-from time import sleep_ms
+import time
 
 DELAYS = {
   'slow': [500, 1000, 1500, 2000],
   'fast': [10, 25, 50, 100]
 }
-RELAX_MAX_TIME = 5000
-FAST_MAX_TIME = 2500
-FAST_CHANCE = 0.01
+
+FAST_CHANCE = 0.1
+FAST_RUN_TIME = 2500
+RELAX_RUN_TIME = 5000
 
 spi = SPI(0,sck=Pin(2),mosi=Pin(3))
 cs = Pin(5, Pin.OUT)
 display = max7219.Matrix8x8(spi, cs, 12)
 display.brightness(1)
 
-fast = False
-time_fast = 0
-relax_time = 0
+class WOPRController:
+  def __init__(self, display):
+    self.display = display
+    self.fast = False
+    self.mode_update_time = 0
+    self.display_update_time = 0
 
-while True:
-  # Check whether we're in frantic mode
-  if not fast and relax_time > RELAX_MAX_TIME:
-    if random.random() <= FAST_CHANCE:
-      fast = True
-  elif fast and time_fast > FAST_MAX_TIME:
-    fast = False
-    time_fast = 0
-    relax_time = 0
+  def update_mode(self):
+    if time.ticks_diff(time.ticks_ms(), controller.mode_update_time) >= 0:
+      self.run_mode()
 
-  # Do the algo
-  for y in range (8):
-    for x in range (96):
-      flip = random.randint(0, 1)
-      if (flip == 0):
-        flipp = random.randint(0, 1)    
-        if (flipp == 0):
-          display.pixel(x,y,1)
-        else:
-          display.pixel(x,y,0)
-      else:
-        pass
-  display.show()
+  def run_mode(self):
+    if self.fast:
+      self.mode_update_time = time.ticks_add(time.ticks_ms(), RELAX_RUN_TIME)
+      self.fast = False
+    else:
+      self.mode_update_time = time.ticks_add(time.ticks_ms(), FAST_RUN_TIME)
+      if random.random() <= FAST_CHANCE:
+        self.fast = True
 
-  # Zzzzz
-  time_sleep = random.choice(DELAYS['fast' if fast else 'slow'])
-  if fast:
-    time_fast += time_sleep
-  else:
-    relax_time += time_sleep
+  def update_display(self):
+    if time.ticks_diff(time.ticks_ms(), self.display_update_time) >= 0:
+      self.run_display()
 
-  sleep_ms(time_sleep)
+  def run_display(self):
+    delay = random.choice(DELAYS['fast' if self.fast else 'slow'])
+    self.display_update_time = time.ticks_add(time.ticks_ms(), delay)
+
+    for y in range(8):
+      for x in range(96):
+        flip = random.randint(0, 1)
+        if flip == 0:
+          flipp = random.randint(0, 1)
+          if flipp == 0:
+            self.display.pixel(x, y, 1)
+          else:
+            self.display.pixel(x, y, 0)
+
+    self.display.show()
+
+  def run(self):
+    while True:
+      self.update_mode()
+      self.update_display()
+
+controller = WOPRController(display)
+controller.run()
